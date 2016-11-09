f5f080ec60aecde7c4ba1bec504abc63d28ff7a5
thomasmeyerh <thom953b@easv365.dk>
2016-11-09 10:31:00 +0100

    Made the targets Refactor the  wombat

---
+++ Angry-Wombats/Slingshot.java
@@ -0,0 +1,188 @@
+ 
+
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Slingshot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Slingshot extends Actor
+{
+    // the images of the slingshot!!
+    GreenfootImage sshot1 = new GreenfootImage("slingshot.png");
+    GreenfootImage sshot2 = new GreenfootImage("slingshot.png");
+    Wombat wombat; // the wombat that gets shot
+    MouseInfo m; // need the mouse info....
+    Strap s1, s2; // the straps of the slingshot
+  
+    Vect displacement = new Vect(0,0); // how far the straps get pulled
+    Vect elasticforce = new Vect(0,0); // the force the slingshot puts on the wombat
+    Vect velocity = new Vect(0,0); // the final velocity of the wombat before it gets launched
+    Vect tempVect = new Vect(0,0); // a temp vect for comparison purposes..see shootwombat
+    Vect anotherVect = new Vect(0,0);  // another vector.  This one is to make sure the straps don't get pulled too far.
+    Vect anotherVectUnit = new Vect(0,0); // the unit vector of another vect
+    TestWorld t; // a pointer to the testworld
+    double wombatmass = 0;
+    
+    // other variables
+    boolean initialized = false;
+    boolean loaded = false;
+    boolean firing = false;
+    boolean fired = false;
+    boolean wombatrectadded = false;
+    boolean wombatrectremoved = false;
+    double mousex;
+    double mousey;
+    double elasticnum = 0.4; // the elastic force constant, the higher this is, the more force
+    double scalar;
+    
+    /**
+     * Act - do whatever the Slingshot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if (!initialized) {
+            initialize();
+        }
+        // adding a wombat to the sling shot makes it loaded
+        if (loaded) {
+            doLoadedStuff(); // I do loaded stuff too, when I drink too much!
+            if (Greenfoot.mouseClicked(null)) {
+                // whoops, we are not loaded anymore!  Wish it was that easy in real life!
+                loaded = false;
+                firing = true;
+            }
+        } 
+        
+        if (firing) {
+            shootWombat(); // shoot the dern thing 
+        }
+    }   
+    
+    public void shootWombat() {
+        // so, accelerate toward where the wombat got loaded, until the wombat passes that
+        // point.  Get the wombat to move, then tell the straps to follow the wombat.
+        // first, get the Force on the wombat.
+        elasticforce = Phyx.elasticForce(displacement,elasticnum);
+        // add the force to the velocity.  Actually I supppose the "correct" way to do this is add the Force/Mass
+        wombatmass = wombat.getMass();
+        velocity = Phyx.getVelocity(velocity,elasticforce,wombatmass); // this method can take mass into account
+        // now that velocity has been computed, we know how fast the wombat is moving.
+        // add the velocity to the displacement, and send the wombat to that new location.
+        // also, move the straps.  Store the displacement before we alter it.
+        tempVect.setEqualTo(displacement); // store the displacement variables here
+        displacement.add(velocity);
+        //wombat.setXandY(getX() + displacement.getX(),(getY()-14) + displacement.getY());
+        s1.setThePoint(getX() + displacement.getX(),(getY()-14) + displacement.getY());
+        s2.setThePoint(getX() + displacement.getX(),(getY()-14) + displacement.getY());
+        // I tried to make the wombat rect move with the wombat, but greenfoot draws them at different times, so it didn't work.
+        // just get rid of the wombat rect for now.
+        
+        wombat.setXandY(getX() + displacement.getX(),(getY()-14) + displacement.getY());
+        // next we see if the wombat has passed the point where it was fired.  The wombat can be fired from
+        // any direction.  Now when fireing, the displacement of the wombat is getting smaller.
+        // if the displacement starts getting bigger, we know we should stop firing.
+        // the displacement was stored in tempVect, so use that to compare.
+        wombat.setXandY(getX() + displacement.getX(),(getY()-14) + displacement.getY());
+        if (tempVect.magnitude() <= displacement.magnitude()) {
+            wombat.release(velocity); // release the wombat at the velocity..
+            // now that the wombat has been released, time to erase the old dots..
+          
+            firing = false; // stop manipulating the wombat...
+            // now reset everything!
+            s1.reset();
+            s2.reset(); // all the booleans should already be reset, so just the straps
+            velocity.setX(0); velocity.setY(0); // set all the vectors back to zero
+            displacement.setX(0); displacement.setY(0);
+            elasticforce.setX(0); elasticforce.setY(0);
+            setImage(sshot1); // set the image back..
+            getWorld().setPaintOrder(Wombat.class,Slingshot.class); // now set the draw order don't forget the dial!
+            wombatrectadded = false;
+            wombatrectremoved = false;
+        }
+        
+    }
+    // this endes up giving control of the wombat to the slingshot, once it gets loaded
+    public void addWombat(Wombat p) {
+        wombat = p;
+        loaded = true;
+        
+    }
+    
+    public void doLoadedStuff()
+    {
+        // alright.  we should make sure the mouse is in the area of the slingshot
+        //if (mouseInNeighborhood()) {
+        //if (!wombatrectadded) {
+        //    wombatrectadded = true;
+        //    getWorld().addObject(pr,0,0);
+        //   getWorld().setPaintOrder(Slingshot.class, WombatRect.class, Wombat.class);
+        //}
+            if (Greenfoot.mouseDragged(null)) {
+              
+                // there is probabily a better way to do this.
+                setImage(sshot2);
+                // getWorld().setPaintOrder(Slingshot.class,Wombat.class); // now draw the slingshot first
+                m = Greenfoot.getMouseInfo();
+                wombat.setXandY(m.getX(),m.getY());
+                mousex = m.getX();
+                mousey = m.getY();
+                anotherVect.setX(-(getX()-mousex));
+                anotherVect.setY(-(getY()-mousey));
+                if (anotherVect.magnitude() <= 90) {
+                    // the wombat gets loaded at about getY()-14.  The displacement is the difference
+                    // between the wombat's position and that location.  
+                    displacement.setX(-(getX() - mousex));  // these should be negitive based on how the
+                    displacement.setY(-(getY()-75 - mousey)); // coord system is set up, didn't catch that at first
+                    //wombat.setXandY(mousex-wombat.getImage().getWidth()/2,mousey+wombat.getImage().getHeight()/2); // move the wombat's location to wherever mousex and mousey is 
+                    //if (displacement.magnitude() <= 90) {
+                    //wombat.setXandY(mousex-wombat.getImage().getWidth()/2,mousey+wombat.getImage().getHeight()/2);
+                    s1.setThePoint(mousex,mousey);
+                    s2.setThePoint(mousex,mousey);
+                    //}
+                    //elasticforce = Phyx.elasticForce(displacement,elasticnum); // so I can see it 
+                } else {
+                    // set the displacement vector, as well as the wombat, at a length of 90 along the mose position vector
+                    displacement.setX(-(getX() - mousex));  
+                    displacement.setY(-(getY()-150 - mousey));
+                    scalar = (90/anotherVect.magnitude());
+                    displacement.scalarMultiply(scalar); // that's the displacement vector....
+                    anotherVect.scalarMultiply(scalar);
+                   // wombat.setXandY(getX()+anotherVect.getX()-wombat.getImage().getWidth()/2,getY()+anotherVect.getY()+wombat.getImage().getHeight()/2);
+                    s1.setThePoint(getX()+anotherVect.getX(),getY()+anotherVect.getY());
+                    s2.setThePoint(getX()+anotherVect.getX(),getY()+anotherVect.getY());
+                    
+                    
+                }
+            //}  
+            // ok this is getting pretty long!  Still not done.  Need a holder for the wombat.  1st step: move the wombat back a couple pixels
+            // toward the slingshot.  another vect already points from the slingshot to where th mouse is, so just use that one
+            anotherVectUnit = anotherVect.returnUnit();
+            anotherVectUnit.scalarMultiply(-5);  // This points from the end of the elastic straps to the slingshot, and is 3 units long
+            wombat.setXandY(getX()+anotherVectUnit.getX()+anotherVect.getX()-wombat.getImage().getWidth()/2,
+                            getY()+anotherVectUnit.getY()+anotherVect.getY()+wombat.getImage().getHeight()/2);
+            anotherVectUnit.scalarMultiply(0.3);
+          
+        }
+    }
+    
+    
+    public void setElasticNum(double input) {
+        elasticnum = input;
+    }
+    
+    public void initialize() {
+        s1 = new Strap();
+        s2 = new Strap();
+        t = (TestWorld) getWorld();
+        getWorld().addObject(s1,getX() +9,getY()-23); // the order in which these are added matters
+        getWorld().addObject(s2,getX() -6,getY()-23); // when it comes to which gets drawn first
+        setImage(sshot1);
+        getWorld().setPaintOrder(Wombat.class,Slingshot.class); // draw the wombat on top of the sling shot
+        initialized = true;
+    }
+}
---
+++ Angry-Wombats/TestWorld.java
@@ -0,0 +1,199 @@
+ 
+
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+/**
+ * Write a description of class TestWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TestWorld  extends World
+{
+    Slingshot s = new Slingshot();
+    boolean wombatadded = false;
+    boolean shotloaded = false;
+    LinkedList <Wombat> wombats = new LinkedList();
+
+    double wombatmass = 5;
+    boolean paused = false;
+    double grav = 0.6; // tell the wombats what gravity is
+    double airfriction = 0.1;
+    Vect windVect = new Vect(0,0);
+    // testing variables
+    boolean test1 = false;
+    boolean test2 = false;
+    double testnum;
+    Vect testVect;
+
+    /**
+     * Constructor for objects of class TestWorld.
+     * 
+     */
+    public TestWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+
+        super(1200, 400, 1); 
+        //GreenfootImage bg = new GreenfootImage("bg.jpg");
+        //setBackground(bg);
+
+        setPaintOrder(Slingshot.class,Wombat.class,Strap.class);
+        addWombats();
+        addObject(s,130,395 - s.getImage().getHeight()/2);
+
+        prepare();
+    }
+
+    public void act() {
+
+        // added a pause feture, to keep things from getting too mixed up.
+        if(!paused) {
+            if (!shotloaded) {
+                if(!wombats.isEmpty()) {
+                    sendToSShot(wombats.element()); // the wombat at the front of the line.  Send it to the shot
+                    shotloaded = true;
+                } else {
+                    endGame();
+                }
+            }
+        }
+
+        // add some targets to shoot at
+
+    }
+
+    public void addWombats() {
+        for (int i = 0 ; i < 5; i++) {
+            Wombat p = new Wombat();
+            wombats.add(p);
+            addObject(p,5*15-(i)*15,50-p.getImage().getHeight()/2);
+        }
+        // wombat at index 4 is at the front of the line.  I wish I could use a queue here.. maybe I can
+        // move the first wombat to the 
+        wombats.element().sendToSShot();
+        sendToSShot(wombats.element());
+    }
+
+    // puts the wombat at the head of the list into the slingshot.  Also gives the slingshot a reference 
+    // to the wombat.
+    public void sendToSShot(Wombat p) {
+        p.setGravity(grav);
+        p.setAirFriction(airfriction);
+        p.sendToSShot();
+        p.setMass(wombatmass);
+        //windVect.scalarMultiply(-1); // for some reason it's all backward.  Just multiply by -1 though
+        p.setWind(windVect); 
+        if(wombats.isEmpty()) {
+            endGame();
+        } else {
+            s.addWombat(p);
+        }
+    }
+
+    public void addAnotherWombat() {
+        // so remove the wombat at the first part of the list, and add another wombat at the back of the list
+        wombats.removeFirst();
+        Wombat p = new Wombat(); // make a new wombat
+        // addObject(p,0,100-p.getImage().getHeight()/2); // add it to the world..
+        // wombats.addLast(p); // add it to the end of the list
+        // now move 'em right...
+        moveRight();
+        shotloaded = false; // and then tell the act() function to load another shot...
+    }
+
+    public void moveRight() {
+        for (Wombat p: wombats) {
+            p.moveRight(1,15);
+        }
+    }
+
+    /**
+     * This method gets input from the inputbox class.  The boxnum indicates how to interpet this input.
+     */
+    public void getInput(int boxnum, double input) {
+        switch (boxnum) {
+            case 1: // this is the gravity one. Gravity is a static int in the PHYX class
+            // actually ended up changing gravity through the wombat, because it's easier that way 
+            // the variable grav gets sent to each wombat as they are loaded into the slingshot
+            /*Phyx.setGravity(input); test1 = true; testVect = Phyx.Gravity(); */grav = input;
+            break; 
+            case 2: // this sets how hard the slingshot accelerates the wombat
+            s.setElasticNum(input); test2 = true;
+            break;
+            case 3: // this sets the mass of the wombat.  
+            wombatmass = input;
+            break;
+            case 4: // this one sets the Air Friction.  Doesn't do anything yet...
+            airfriction = input; // now it does!
+            break;
+
+        }
+    }
+
+    public void endGame() {
+        Greenfoot.stop();
+    }
+
+    public void Pause() {
+        paused = true;
+    }
+
+    public void unPause() {
+        paused = false;
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Stone stone = new Stone();
+        addObject(stone,1161,367);
+        stone.setLocation(1169,361);
+        Stone stone2 = new Stone();
+        addObject(stone2,1075,368);
+        stone2.setLocation(1049,360);
+        Stone stone3 = new Stone();
+        addObject(stone3,929,360);
+        Stone stone4 = new Stone();
+        addObject(stone4,1050,266);
+        Stone stone5 = new Stone();
+        addObject(stone5,1151,255);
+        Stone stone6 = new Stone();
+        addObject(stone6,912,260);
+        WoodVertical woodvertical = new WoodVertical();
+        addObject(woodvertical,1105,369);
+        WoodVertical woodvertical2 = new WoodVertical();
+        addObject(woodvertical2,987,362);
+        WoodVertical woodvertical3 = new WoodVertical();
+        addObject(woodvertical3,971,265);
+        WoodVertical woodvertical4 = new WoodVertical();
+        addObject(woodvertical4,1004,248);
+        Wood wood = new Wood();
+        addObject(wood,1146,307);
+        Wood wood2 = new Wood();
+        addObject(wood2,867,314);
+        Wood wood3 = new Wood();
+        addObject(wood3,1027,147);
+        wood.setLocation(1169,292);
+        stone5.setLocation(1166,221);
+        woodvertical4.setLocation(1094,220);
+        stone4.setLocation(1023,221);
+        woodvertical2.setLocation(867,346);
+        stone2.setLocation(1031,367);
+        stone2.setLocation(1006,366);
+        woodvertical.setLocation(1099,364);
+        stone2.setLocation(1026,363);
+        wood3.setLocation(1023,293);
+        woodvertical3.setLocation(951,221);
+        stone6.setLocation(878,221);
+        wood2.setLocation(877,293);
+        stone3.setLocation(879,366);
+        woodvertical2.setLocation(954,363);
+        stone3.setLocation(878,362);
+    }
+}
